c4b693ad641fa2e0fb3d2fa13bd5de77d049ab58
Paty Bruno <paty@intechinfo.fr>
2013-05-14 17:59:49 +0200

    add a enum Status to control the visibility of lineItems

---
+++ CiView2/Viewer/Model/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Model
+{
+    public enum Status
+    {
+        Callapsed,
+        Expended,
+        Hidden,
+    }
+}
