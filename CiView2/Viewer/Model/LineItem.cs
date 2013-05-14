83414a69980d949d2ac52da96aba55368020d9c6
Paty Bruno <paty@intechinfo.fr>
2013-05-14 17:53:29 +0200

    Initiate the viewer Model
    Create an abstract class LineItem
    Create 2 class : filtredLineItem and LogLineItem
    
    filtredLineItem is a class that is structured the visual data
    LigLineItem is a class that contains the data

---
+++ CiView2/Viewer/Model/LineItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Model
+{
+    public abstract class LineItem
+    {
+        #region Properties
+        public BagItems Host { get; private set; }
+        public LineItem Parent { get; internal set; }
+        public LineItem Next { get; internal set; }
+        public LineItem FirstChild { get; internal set; }
+        public LineItem LastChild { get; internal set; }
+        public LineItem Previous { get; internal set; }
+        public int Depth { get; internal set; }
+        public int LineHeight { get; set; }
+        public int TotalLineHeight { get; set; }
+        public Status status;
+        public int AbsoluteY;
+        public int LocalY;
+        public int ID;
+        public LineItem NextError;
+        #endregion
+    }
+}
---
+++ CiView2/Viewer/Model/LogLineItem.cs
@@ -0,0 +1,29 @@
+﻿using CK.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Model
+{
+    class LogLineItem : LineItem
+    {
+        public String content;
+        public LogLevel loglevel;
+        public CKTrait trait;
+
+        public LogLineItem(String content, LogLevel loglevel, BagItems host, Status status, CKTrait trait)
+        {
+        }
+        public void InsertChildren(List<LogLineItem> children)
+        {
+        }
+        public void insertChild(LogLineItem child)
+        {
+        }
+        public void Delete()
+        {
+        }
+    }
+}
