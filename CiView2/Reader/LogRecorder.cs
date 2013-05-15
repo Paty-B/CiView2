1b127f8394c0502a65fb0f6b992d91315c9b85d3
Benjamin Gratade <gratade@intechinfo.fr>
2013-05-15 09:45:11 +0200

    Add speed reading methods

--- CiView2/Reader/LogRecorder.cs
+++ CiView2/Reader/LogRecorder.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Reader
 {
@@ -30,6 +31,44 @@ namespace Reader
                 _logs.Add(temp);
             }
             _readerHelper.Free();
+        }
+
+        public void ReadLogsFileOntime(double speed = 1)
+        {
+            LogData pastLog;
+            LogData currentLog;
+
+            Timer timer = new Timer();
+            timer.AutoReset = false;
+            timer.Enabled = false;
+
+            pastLog = _readerHelper.Read();
+            currentLog = _readerHelper.Read();
+            SendToViewer(pastLog);
+            do
+            {
+                Wait(pastLog, currentLog, timer, speed);
+                SendToViewer(currentLog);
+                pastLog = currentLog;
+
+            } while ((currentLog = _readerHelper.Read()) != null);
+
+            timer.Close();
+            _readerHelper.Free();
+        }
+
+        private void Wait(LogData pastLog, LogData currentLog, Timer timer, Double speed = 1)
+        {
+            timer.Interval = speed * ((currentLog._date - pastLog._date).TotalMilliseconds);
+            timer.Start();
+            while (timer.Enabled != false) { /*Simulating time*/}
+        }
+
+        private void SendToViewer(LogData log)
+        {
+            Console.WriteLine("{0} Tag: {1}", log._date, log._tag);
+
+            //  throw new NotImplementedException();
         }
     }
 }
