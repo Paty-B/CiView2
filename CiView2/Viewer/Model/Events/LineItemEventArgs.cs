05477dbf2b2163c75052876cc890871bd72973fa
Paty Bruno <paty@intechinfo.fr>
2013-06-10 14:21:31 +0200

    add event class

---
+++ CiView2/Viewer/Model/Events/LineItemEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.Model
+{
+
+    public enum LineItemChangedStatus
+    {
+        Hidden,
+        Expanded,
+        Collapsed,
+        Deleted,
+        Inserted
+    }
+
+    public class LineItemChangedEventArgs : EventArgs
+    {
+        public readonly ILineItem LineItem;
+        public readonly LineItemChangedStatus Status;
+
+        public LineItemChangedEventArgs(ILineItem line, LineItemChangedStatus status)
+        {
+            LineItem = line;
+            Status = status;
+        }
+    }
+}
